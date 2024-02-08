ff240cc6249d9e2ef9441a19684d069f67f1e84f
lyndemberggit <lyndembergnrodrigues@yahoo.com.br>
2024-02-08 16:56:02 -0300

    novo

--- Módulo 02/07 - Interação com API/Aula 03/Exemplo 03/Exemplo-01.js
+++ Módulo 02/07 - Interação com API/Aula 03/Exemplo 03/Exemplo-01.js
@@ -1,11 +1,61 @@
-async function obtendo(){
+/*async function obtendo(){
     let resposta = await fetch("Exemplo-01.txt")
     let convertido = await resposta.text()
     console.log(convertido)
 
-    alert(convertido)
+   // alert(convertido)
+  // document.body.innerHTML = "<h1>" + convertido + "</h1>"*/
+ 
+ 
 
+  /*for(let x = 0; x <= 3; x++){
+    document.body.innerHTML += `
+    <h1>
+          ${convertido} 
+    </h1>)
+    `//linha 8 a 12, metodo mais usado
+ 
+ 
+    
+  }
+  
+
+    obtendo()*/
+/*async function obtendo(){
+    let resposta = await fetch("Exemplo-01.txt")
+    let convertido = await resposta.text()
+
+    let lista = ["luana", "patricia", "lais"]
+    
+    for(let x in lista){
+    document.body.innerHTML+= `
+    <h1>
+        ${convertido}
+    </h1>
+    `
     }
-    obtendo()
+ 
+}*/
 
+async function obtendo(){
+    let resposta = await fetch("Exemplo-01.txt")
+    let convertido = await resposta.text()
+
+    let lista = ["luana", "patricia", "lais"]
+    
+    for(let x in lista){
+    document.body.innerHTML+= `
+    <h1>
+        ${convertido}
+    </h1>
+    <p>
+    ${lista[x]}
+    </p>
+    `
+    }
+ 
+}
+
+
+  
 
