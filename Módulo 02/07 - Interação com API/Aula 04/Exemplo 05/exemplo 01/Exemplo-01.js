f1d86a108eb587aa3e4f9a945698f625d59ea4e0
lyndemberggit <lyndembergnrodrigues@yahoo.com.br>
2024-02-20 16:42:42 -0300

    nova20/0224

--- Módulo 02/07 - Interação com API/Aula 04/Exemplo 05/exemplo 01/Exemplo-01.js
+++ Módulo 02/07 - Interação com API/Aula 04/Exemplo 05/exemplo 01/Exemplo-01.js
@@ -1,9 +1,33 @@
-async function resolver(){
+/*async function resolver(){
     let busca1 = await fetch ("tarefas.json")
     let tarefas = await busca1.json()
 
     for(let x in tarefas){
         document.body.innerHTML+= `<h1> ${tarefas[x]}</h1>`
+    }
+
+    
+
+    
+
+}
+resolver()*/
+
+async function resolver(){
+    let busca1 = await fetch("tarefas.json")
+    let tarefas = await busca1.json()
+
+    let busca2 = await fetch("cores.json")
+    
+    let cores = await busca2.json()
+    
+
+    for(let x of tarefas){
+        let corAleatoria = parseInt(Math.random() * 7)
+        document.body.innerHTML+= `
+        <h1 style="color:${cores[corAleatoria]} red"> ${x}</h1>
+        
+        `
     }
 
     
